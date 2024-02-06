8195abf5d4c40f353dba60c2a023b8693c34ace9
shehriar <shehriar1burney@gmail.com>
2024-02-07 02:26:58 +0500

    Made changes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,51 +2,53 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; 
 import { Observable } from 'rxjs';
 import { LocationService } from './location.service';
-  
+import { environment } from 'src/environments/environment';
+
+
 @Injectable({ 
     providedIn: 'root'
 }) 
 export class ApiService { 
+    private apiUrl = environment.backendUrl;
     constructor(private http: HttpClient) { }
     getMessage() { 
-        return this.http.get( 
-            'http://localhost:3000/api/message'); 
+        return this.http.get(`${this.apiUrl}/api/message`);
     } 
     getLocations():Observable<any>{
         return this.http.get(
-            'http://localhost:3000/api/locations'
+            `${this.apiUrl}/api/locations`
         )
     }
 
     getAllCarModels():Observable<any>{
         return this.http.get(
-            'http://localhost:3000/api/vehicle_models'
+            `${this.apiUrl}/api/vehicle_models`
         )
     }
 
     getVehicleDataFromPickupLocation(pickupLocation:string):Observable<any>{
         console.log(pickupLocation);
         return this.http.post(
-            'http://localhost:3000/api/vehicles_from_pickup_location',
+            `${this.apiUrl}/api/vehicles_from_pickup_location`,
             { pickupLocation: pickupLocation }
         );
     }
 
     getVehicleTypes():Observable<any>{
         return this.http.get(
-            'http://localhost:3000/api/vehicle_types'
+            `${this.apiUrl}/api/vehicle_types`
         )
     }
 
     getVehicleMakes():Observable<any>{
         return this.http.get(
-            'http://localhost:3000/api/vehicle_makes'
+            `${this.apiUrl}/api/vehicle_makes`
         )
     }
 
     getFuelTypes():Observable<any>{
         return this.http.get(
-            'http://localhost:3000/api/fuel_types'
+            `${this.apiUrl}/api/fuel_types`
         )
     }
 }
